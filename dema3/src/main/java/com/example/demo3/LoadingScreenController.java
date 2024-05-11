531eda96fc87212cc93aaf394aa72e2108ebef9b
Oshan Harshad <oshanedu@gmail.com>
2024-05-11 16:11:50 +0530

    Loading Screen Added

---
+++ dema3/src/main/java/com/example/demo3/LoadingScreenController.java
@@ -0,0 +1,69 @@
+package com.example.demo3;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.stage.Window;
+
+import java.io.IOException;
+
+
+public class LoadingScreenController {
+
+    @FXML
+    private Label loadingPercentage;
+
+    @FXML
+    private Rectangle recMain;
+
+    @FXML
+    private Rectangle recSub;
+
+
+    public void initialize() {
+
+        LoadingTask task=new LoadingTask();
+
+        task.progressProperty().addListener((obv, ov, nw) -> {
+
+            String fs=String.format("%.0f", nw.doubleValue()*100);
+            System.out.println(fs);
+            loadingPercentage.setText(fs+"%");
+
+            recSub.setWidth(recMain.getWidth()*nw.doubleValue());
+
+            if(nw.doubleValue()==1.0){
+                Window window=loadingPercentage.getScene().getWindow();
+                Stage s=(Stage) window;
+                s.close();
+
+                FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login.fxml"));
+                Scene scene=null;
+                try {
+                    scene = new Scene(fxmlLoader.load(), 600, 400);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                Stage stage=new Stage();
+                stage.setScene(scene);
+                //stage.initStyle(StageStyle.UNDECORATED);
+
+
+                stage.show();
+
+            }
+
+
+        });
+
+        new Thread(task).start();
+    }
+
+
+
+
+}
---
+++ dema3/src/main/java/com/example/demo3/Starter.java
@@ -0,0 +1,7 @@
+package com.example.demo3;
+
+public class Starter {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
